92129ddf04b07cde8af0cb2a64391f5031df4d2b
Uwe L. Korn <uwelk@xhochy.org>
2009-10-25 23:16:52 +0100

    Defined all handlers

--- src/server.cpp
+++ src/server.cpp
@@ -11,6 +11,10 @@ void jsonStats(const request&, Response&)
 int main(int argc, char * argv[])
 {
   Hochheim::Server<FullPathHandler> server("127.0.0.1", "8585", 20);
-  //server::requestHander().addHandler("/statistics.json", jsonStats);
+  server.requestHander().addHandler("/statistics.json", jsonStats);
+  server.requestHander().addHandler("/user-info.json", jsonStats);
+  server.requestHander().addHandler("/big-chart.png", jsonStats);
+  server.requestHander().addHandler("/small-chart.png", jsonStats);
+  server.requestHander().addHandler("/list-list.png", jsonStats);
   server.run();
 }
